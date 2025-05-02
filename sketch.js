1303b93b75e5b168e652201e9c5b7327fc2d0751
yanwang010 <wangyan940916@gmail.com>
2025-05-02 11:05:49 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,13 +18,6 @@ function draw() {
   // 設定 graphics 的背景為黑色
   graphics.background(0);
 
-  // 水平翻轉 graphics 並繪製 capture 的影像
-  graphics.push(); // 儲存當前狀態
-  graphics.translate(graphics.width, 0); // 將原點移到右側
-  graphics.scale(-1, 1); // 水平翻轉
-  graphics.image(capture, 0, 0, graphics.width, graphics.height); // 繪製影像
-  graphics.pop(); // 恢復狀態
-
   // 在 graphics 上繪製圓形，顏色來自 capture 的相對位置
   for (let x = 0; x < graphics.width; x += 20) {
     for (let y = 0; y < graphics.height; y += 20) {
@@ -50,4 +43,15 @@ function windowResized() {
 
   // 重新建立與 video 畫面相同大小的圖形緩衝區
   graphics = createGraphics(capture.width, capture.height);
+
+  // 將影像繪製到圖形緩衝區，並左右翻轉
+  graphics.push(); // 儲存當前狀態
+  graphics.translate(graphics.width, 0); // 將原點移到右側
+  graphics.scale(-1, 1); // 水平翻轉
+  graphics.image(capture, 0, 0, graphics.width, graphics.height); // 繪製影像
+  graphics.pop(); // 恢復狀態
+
+
+
+
 }
